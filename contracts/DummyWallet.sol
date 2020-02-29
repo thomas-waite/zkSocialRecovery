10ed9b11beeed6df58745a8ae1c9670fb3ef881a
Makoto <inouemak@googlemail.com>
2020-02-29 15:10:07 +0000

    Modify DummyWallet

--- contracts/DummyWallet.sol
+++ contracts/DummyWallet.sol
@@ -2,6 +2,12 @@ pragma solidity ^0.6.1;
 
 
 contract DummyWallet {
+    uint8 public _recoveryThreshold;
+    bool public recovered;
+
+    constructor(address payable _owner, bool _transferable, uint8 _threshold) public {
+        _recoveryThreshold = _threshold;
+    }
 
     mapping (bytes32 => bool) public zkMerlins;
 
@@ -10,6 +16,8 @@ contract DummyWallet {
     }
 
     function zkRecover(address payable _recoveryAddress, bytes32 _proofHash, bytes memory _proofData) public returns (bool) {
+        recovered = true;
+        return recovered;
     }
     
     function transfer(address payable _to,  uint _amount) public {
